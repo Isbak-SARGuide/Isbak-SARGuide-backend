34e05dc32508ed2c43844e8ba5d3d480907fe4a7
Eren atasoy <erenatsaoy04@gmail.com>
2026-08-24 08:24:14 +0300

    chore: packages, test project, and namespace fix
    - Added namespace to ApplicationUser (global namespace issue)
    - Installed Mapster, FluentValidation, Serilog, Asp.Versioning, Scalar
    - Created tests/Isbak_SAR_Guide.Tests (xUnit + Shouldly + NSubstitute + Testcontainers)
    - Added 2 tests verifying Identity inheritance

--- AramaKurtarma.Entities/Identity/ApplicationUser.cs
+++ AramaKurtarma.Entities/Identity/ApplicationUser.cs
@@ -1,10 +1,11 @@
-
 using Microsoft.AspNetCore.Identity;
 
 namespace AramaKurtarma.Entities.Identity;
 
+
 public class ApplicationUser : IdentityUser
 {
     public string FullName { get; set; } = null!;
+
     public DateTime CreatedAt { get; set; }
 }
